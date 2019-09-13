6543c93d43c5a6572999b4c63b4c27cf0eb951cf
Matthias Grieder <matthias@zeitvertrieb.net>
2019-09-13 16:25:08 +0200

    Updates

--- sw.js
+++ sw.js
@@ -32,7 +32,7 @@ self.__precacheManifest = [
     "url": "commons.a8245a60852abddaae14.css"
   },
   {
-    "url": "commons-365ba2877761a4142406.js"
+    "url": "commons-e24e70690ab4fe2d98b5.js"
   },
   {
     "url": "app-90cf83d255805f518760.js"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "02d0d4a8152e96589cdb45af0fe0ae05"
+    "revision": "0faf99ca4e4cc76ba3953102e11cf8e2"
   },
   {
     "url": "manifest.webmanifest",
