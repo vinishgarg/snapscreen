74efab9db161054a48deefc3becafa28b37db76e
Matthias Grieder <matthias.grieder@ebcont.com>
2020-07-24 10:39:15 +0200

    sw change

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,11 +1,1 @@
-// Service worker update message
-export const onServiceWorkerUpdateReady = () => {
-  const answer = window.confirm(
-    `Snapscreen has been updated. ` +
-      `Reload to display the latest version?`
-  )
 
-  if (answer === true) {
-    window.location.reload()
-  }
-}
