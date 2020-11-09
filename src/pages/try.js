5c029a07320b54ad8d018de1e90c128e00c214bf
Matthias Grieder <matthias.grieder@ebcont.com>
2020-11-09 14:19:22 +0100

    update try.js

--- src/pages/try.js
+++ src/pages/try.js
@@ -30,9 +30,9 @@ const Try = ({ intl }) => {
                 <div className="text-center p-3">
                   <svg xmlns='http://www.w3.org/2000/svg' width='128' height='128' viewBox='0 0 512 512'>
                     <title><FormattedMessage id={"offline.status"} /></title>
-                    <path d='M93.72,183.25C49.49,198.05,16,233.1,16,288c0,66,54,112,120,112H320.37' style={{fill: 'none', stroke: '#fff', strokeLinecap: 'round', strokeLinejoin:'round', strokeWidth:'32px'}} />
-                    <path d='M467.82,377.74C485.24,363.3,496,341.61,496,312c0-59.82-53-85.76-96-88-8.89-89.54-71-144-144-144-26.16,0-48.79,6.93-67.6,18.14' style={{fill: 'none', stroke: '#fff', strokeLinecap: 'round', strokeLinejoin: 'round', strokeWidth:'32px'}} />
-                    <line x1='448' y1='448' x2='64' y2='64' style={{fill:'none', stroke: '#fff', strokeLinecap: 'round', strokeMiterlimit:10, strokeWidth: '32px'}}/>
+                    <path d='M93.72,183.25C49.49,198.05,16,233.1,16,288c0,66,54,112,120,112H320.37' style={{fill: 'none', stroke: 'var(--base-text-color)', strokeLinecap: 'round', strokeLinejoin:'round', strokeWidth:'32px'}} />
+                    <path d='M467.82,377.74C485.24,363.3,496,341.61,496,312c0-59.82-53-85.76-96-88-8.89-89.54-71-144-144-144-26.16,0-48.79,6.93-67.6,18.14' style={{fill: 'none', stroke: 'var(--base-text-color)', strokeLinecap: 'round', strokeLinejoin: 'round', strokeWidth:'32px'}} />
+                    <line x1='448' y1='448' x2='64' y2='64' style={{fill:'none', stroke: 'var(--base-text-color)', strokeLinecap: 'round', strokeMiterlimit:10, strokeWidth: '32px'}}/>
                   </svg>
                 </div>
                 <h1 className="display-2 my-4 py-4 text-center" style={{minHeight: 0}}>
@@ -83,7 +83,7 @@ const Try = ({ intl }) => {
                        />
                   </Col>
                   <Col xs="2" sm="1" md="1" className="my-auto">
-                    <ion-icon size="large" name="phone-portrait-outline" style={{color: "#ffffff", display: "inline-flex"}} />
+                    <ion-icon size="large" name="phone-portrait-outline" style={{color: "var(--base-text-color)", display: "inline-flex"}} />
                   </Col>
                   <Col xs="6" sm="6" md="7" className="mr-auto">
                     <p className="mt-4">
